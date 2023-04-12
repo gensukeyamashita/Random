0a904acfee18fc46f5db42e6566f644aa9cee05f
山下源助 <yamashiita@gmail.com>
2023-04-12 11:27:11 +0900

    Springboot Project. Spring batch project  with oracledb driver.

---
+++ springboot/batchandoracle/batchandoracleexample/src/main/java/com/example/batchandoracleexample/BatchandoracleexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchandoracleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchandoracleexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchandoracleexampleApplication.class, args);
+    }
+
+}
---
+++ springboot/batchandoracle/batchandoracleexample/src/test/java/com/example/batchandoracleexample/BatchandoracleexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchandoracleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchandoracleexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
